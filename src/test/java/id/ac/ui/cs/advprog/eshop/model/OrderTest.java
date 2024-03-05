4a5788133f3bf52f3fc2b88948979877af4cd17d
ArvinCS <arvinciu86@yahoo.com>
2024-03-05 22:52:19 +0700

    [RED] Add Order model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -33,7 +33,7 @@ public class OrderTest {
     void testCreateOrderEmptyProduct() {
         this.products.clear();
 
-        assertThrows(IllegalArgumentException.class, () => {
+        assertThrows(IllegalArgumentException.class, () -> {
             Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                 this.products, 1708560000L, "Safira Sudrajat");
         });
